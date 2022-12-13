182af22744f3c5c627043540fb04ff3db594a902
rubiyet <rubiyetfardous@gmail.com>
2022-12-13 14:42:37 +0600

    card component added

--- components/card/index.jsx
+++ components/card/index.jsx
@@ -1,7 +1,7 @@
 export default function Card({ taskTitle }) {
   return (
-    <div className="w-96 h-12 px-2 truncate border-2">
-      ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+    <div className="w-96 h-12 p-2 truncate border-2">
+      <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam accumsan vel augue vulputate malesuada.</span>
     </div>
   );
 }
