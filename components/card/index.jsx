e0f85f13350e31b60d9d31a57214152d7dae4ad9
rubiyet <rubiyetfardous@gmail.com>
2022-12-13 14:34:56 +0600

    project created

---
+++ components/card/index.jsx
@@ -0,0 +1,7 @@
+export default function Card({ taskTitle }) {
+  return (
+    <div className="w-96 h-12 px-2 truncate border-2">
+      ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+    </div>
+  );
+}
